6203d6edfb738b3d26b2c7666600207a7f0a7db3
USER <USER@192.168.1.147>
2021-02-16 12:35:30 +0000

    polyfit_ph update: new options (start, stop)

--- functions/Sequence/polyfit_ph.m
+++ functions/Sequence/polyfit_ph.m
@@ -1,48 +1,64 @@
-function phi_corr = polyfit_ph(pulse, Phase, Options)
+function phi_corr = polyfit_ph(pulse, phase, opt)
 % Computes the phase correction to obtain a uniform phase with a
 % polynomial fitting
 %     
 % Input:
 %     - pulse, the pulse onto which to apply the phase correction
-%     - Phase, vector containing the phases of each magnetization point
-%     - Options, field containing two optional parameters:
-%           - deg, degree of the polynomial for the fit
-%           - disp, option to set to true to display the polyfit results
+%     - phase, vector containing the phases of each magnetization point
+%     - opt, field containing optional parameters:
+%           - polyfit_degree, degree of the polynomial for the fit
+%           - display_result, option to set to true to display the polyfit
+%             results
+%           - start, percentage between 0 and 100 corresponding to the
+%           starting point on phase for the polynomial fitting
+%           -stop, cf. start but for the stopping point.
 %
 % Output:
 %     - phi_corr, array containing the phase corrections in degrees to be
 %     applied to each point of the pulse (in the unsmoothed part).
 
 
-grumble(pulse, Phase, Options);
+grumble(pulse, phase, opt);
 
-if isfield(Options, 'polyfit_degree')
-    deg = Options.polyfit_degree;
+if isfield(opt, 'polyfit_degree')
+    deg = opt.polyfit_degree;
 else
-    deg = 10;
+    deg = 6;
 end
 
-% unsmoothed part beginning index for the magnetization phase vector
-i_sm_Phase = floor(length(Phase) * pulse.sm /100);
-% beginning and end of smoothing
-begin_sm_Phase = i_sm_Phase;
-end_sm_Phase = length(Phase) - i_sm_Phase;
+% start and stop point picked at smoothing by default
+if isfield(opt, 'start')
+    start = opt.start;
+else
+    start = pulse.sm;
+end
+
+if isfield(opt, 'stop')
+    stop = opt.stop;
+else
+    stop = pulse.sm;
+end
+
+% start index
+i_start = floor(length(phase) * start /100);
+% stop index
+i_stop = length(phase) - floor(length(phase) * stop /100);
 
 % unsmoothed part selection for the magnetization phase vector
-xdata = begin_sm_Phase:end_sm_Phase;
-ydata = Phase(begin_sm_Phase:end_sm_Phase);
+xdata = i_start:i_stop;
+ydata = phase(i_start:i_stop);
 
 % S and mu: scaling factor used by matlab
 % (to avoid warning, needs to be used with polyval too)
 [pol, S, mu] = polyfit(xdata, ydata, deg);
 
 % calculate the phase correction for the pulse phase vector
-x_phi_corr = linspace(1, length(Phase), pulse.np);
+x_phi_corr = linspace(1, length(phase), pulse.np);
 phi_corr = polyval(pol, x_phi_corr, S, mu);
 
 % optional display
-if isfield(Options, 'display_result')
-    if Options.display_result == true
+if isfield(opt, 'display_result')
+    if opt.display_result == true
         
         yfit = polyval(pol, xdata, S, mu);
         figure();
@@ -66,11 +82,19 @@ end
 
 end
 
-function grumble(pulse, Phase, Options)
+function grumble(pulse, phase, opt)
 
-if isfield(Options, 'display_result')
-    if ~islogical(Options.display_result)
+if isfield(opt, 'display_result')
+    if ~islogical(opt.display_result)
         error('display_result must be a boolean')
+    end
+end
+
+if isfield(opt, 'polyfit_degree')
+    if ~isreal(opt.polyfit_degree) || ...
+            floor(opt.polyfit_degree) ~= opt.polyfit_degree || ...
+            opt.polyfit_degree <= 0
+        error('polyfit_degree must be an integer > 0')
     end
 end
 
